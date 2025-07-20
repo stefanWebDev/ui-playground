e81b4701f880cf1cc6150e0b9baaeb470e8cfcd3
Stefan Lazariotis <stefan.lazariotis@gmail.com>
2025-07-20 18:27:46 +0200

    added formData logic

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,10 +1,26 @@
+import { useFormData } from "@/components/form/hook";
+import Input from "@/components/form/Input";
 
 
 
 export default function Login() {
+  const [formData, setFormField] = useFormData();
+
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-     Login
+      Login
+
+      <form className="w-full max-w-md">
+        <Input onChange={(e) => setFormField("email", e.target.value)} label="Email" />
+        <Input onChange={(e) => setFormField("password", e.target.value)} label="Password" />
+
+        <button
+          type="submit"
+          className="mt-4 w-full bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
+        >
+          Sign Up
+        </button>
+      </form>
     </div>
   );
 }
--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,10 +1,32 @@
+"use client";
 
+import { useFormData } from "@/components/form/hook";
+import Input from "@/components/form/Input";
 
 
 export default function Signup() {
+  const [formData, setFormField] = useFormData();
+
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-     Signups
+      Signup
+
+      <form className="w-full max-w-md">
+        <Input onChange={(e) => setFormField("surname", e.target.value)} label="Surname" />
+        <Input onChange={(e) => setFormField("name", e.target.value)}  label="Name" />
+        <Input onChange={(e) => setFormField("city", e.target.value)}  label="City" />
+        <Input onChange={(e) => setFormField("address", e.target.value)}  label="Address" />
+        <Input onChange={(e) => setFormField("email", e.target.value)}  label="Email" />
+        <Input onChange={(e) => setFormField("password", e.target.value)}  label="Password" />
+
+        <button
+          type="submit"
+          className="mt-4 w-full bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
+        >
+          Sign Up
+        </button>
+      </form>
+
     </div>
   );
 }
