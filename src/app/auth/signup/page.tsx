af7e5dbd370489fb9e20d5359f351787945b70ca
Stefan Lazariotis <stefan.lazariotis@gmail.com>
2025-07-20 17:33:56 +0200

    initial commit

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
 
-export default function Home() {
+
+
+export default function Signup() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-     Main
+     Signup
     </div>
   );
 }
