d677d2f5a9ba9edde753119e6f01effe7eacbbfa
Stefan Lazariotis <stefan.lazariotis@gmail.com>
2025-07-22 09:08:17 +0200

    styled list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,9 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="font-sans p-8 flex flex-col gap-8">
-      <h2 className="text-lg">UI Components:</h2>
+      <h1 className="text-3xl font-bold mb-4 tracking-tight">
+        UI Components:
+      </h1>
       <List items={[
         { id: "1", label: "Sign up", link: "/auth/signup" },
         { id: "2", label: "Sign in", link: "/auth/signin" },
--- src/components/List.tsx
+++ src/components/List.tsx
@@ -12,10 +12,10 @@ interface ListProps {
 
 export const List = ({ items }: ListProps) => {
   return (
-    <ul>
+    <ul className="flex flex-col gap-4">
       {items.map((item) => (
-        <li key={item.id}>
-          <Link href={item.link}>
+        <li className="hover:bg-[var(--background)] hover:opacity-100 text-center cursor-pointer h-10 w-1/2 text-xl font-semibold opacity-80 bg-[#f9f6f0] border-2 border-[var(--chocolate)] rounded-md" key={item.id}>
+          <Link className="h-full w-full block" href={item.link}>
             {item.label}
           </Link>
         </li>
