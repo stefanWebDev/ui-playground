6385887b6ca86fa3c5fc328c823562edc2395e95
Stefan Lazariotis <stefan.lazariotis@gmail.com>
2025-07-22 21:44:48 +0200

    cleanup

--- src/app/api/beacon/route.ts
+++ src/app/api/beacon/route.ts
@@ -2,7 +2,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 import crypto from 'crypto';
 
-
 export async function POST(request: NextRequest) {
 
   const data = await request.json();
@@ -10,7 +9,6 @@ export async function POST(request: NextRequest) {
   const hash = crypto.createHash('sha256').update(fingerprint).digest('hex');
 
   console.log("Beacon received: user changed page, tracking user journey, url:", data.url, ". Hashed fingerprint:", hash);
-
 
   return NextResponse.json(
     { message: 'Dummy reponse, not needed for sendBeacon but reuquired for HTTP standard' },
