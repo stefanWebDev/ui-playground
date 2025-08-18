3e915c2331c704ad39425666ee7598646de7273f
Stefan Lazariotis <stefan.lazariotis@gmail.com>
2025-08-18 14:39:28 +0200

    defined route for iot data fetching

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,19 @@
 "use client";
 
+import { useQuery } from "@tanstack/react-query";
+
 export default function Home() {
+  const { data: user, isLoading } = useQuery({
+    queryKey: ["iot-data"],
+    queryFn: async () => {
+      const response = await fetch("/api/private/iot-data");
+
+      if (!response.ok) return null;
+
+      return await response.json();
+    },
+  });
+
   return (
     <div className="font-sans p-8 flex flex-col gap-8">
       <div className="flex justify-between items-center">
