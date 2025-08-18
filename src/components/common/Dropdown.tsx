ed8676ac6070c2bdcbdd8ee172ad4dba29c06056
Stefan Lazariotis <stefan.lazariotis@gmail.com>
2025-08-18 09:41:17 +0200

    moved dropdown

---
+++ src/components/common/Dropdown.tsx
@@ -0,0 +1,20 @@
+import { SelectHTMLAttributes } from "react";
+
+interface DropdownProps extends SelectHTMLAttributes<HTMLSelectElement> {
+  options: { value: string; label: string }[];
+}
+
+export const Dropdown = ({ options, ...rest }: DropdownProps) => {
+  return (
+    <select
+      className="px-4 py-2 rounded border border-gray-300 focus:border-transparent text-gray-800 focus:outline-none focus:ring-2 focus:ring-[var(--accent-color)] transition"
+      {...rest}
+    >
+      {options.map((option) => (
+        <option key={option.value} value={option.value}>
+          {option.label}
+        </option>
+      ))}
+    </select>
+  );
+};
