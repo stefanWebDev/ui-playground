270efcda09846d5147448c94713b4e2f6837a644
Stefan Lazariotis <stefan.lazariotis@gmail.com>
2025-08-20 11:29:58 +0200

    defined route to persist data

--- src/app/api/mqtt/[topic]/route.ts
+++ src/app/api/mqtt/[topic]/route.ts
@@ -1,6 +1,28 @@
+import { PrismaClient } from "@/generated/prisma/default";
+import { IoTDataSchema } from "@/types/schema";
 import { NextRequest, NextResponse } from "next/server";
 
+const prisma = new PrismaClient();
+
 export async function POST(req: NextRequest) {
-  const body = await req.json();
-  return NextResponse.json({ success: true, received: body.value });
+  const parsed = IoTDataSchema.safeParse(req.body);
+
+  if (parsed.error) {
+    return NextResponse.json({
+      message: "Invalid data",
+      error: "Validation failed",
+      status: 400,
+    });
+  }
+
+  const data = parsed.data;
+
+  await prisma.observation.create({
+    data: {
+      sensorId: data.sensorId,
+      value: data.ObservationValue,
+    },
+  });
+
+  return NextResponse.json({ success: true, received: parsed.data });
 }
